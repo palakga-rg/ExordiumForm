2291ba17fa1147c2b25ddb3d5f2a41443e50fb05
Kushagrekaushik <kkaushik_be21@thapar.edu>
2023-10-23 17:28:48 +0000

    changes

--- website/views.py
+++ website/views.py
@@ -80,7 +80,7 @@ def join(request):
         #             spamwriter.writerow(data1)      
 
 
-        with open('participants1.csv', 'a', newline='') as csvfile:                 
+        with open('/home/Exordium/ExordiumForm/participants1.csv', 'a', newline='') as csvfile:                 
                     spamwriter= csv.writer(csvfile)
                     data1=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12]
     
